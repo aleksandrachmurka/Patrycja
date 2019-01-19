e9bce4d5982fef7ed139f7f16b97571a8f04976d
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-01-19 16:53:59 +0100

    Fix background position

---
+++ index.js
@@ -0,0 +1,7 @@
+const body= document.querySelector('body');
+const content= document.querySelector('.content');
+const shows = document.querySelector('.shows');
+const gallery = document.querySelector('.gallery');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact');
+
