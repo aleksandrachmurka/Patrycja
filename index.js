d6732b7ed6a19dd0d9b3543c3370fc21e960a3fe
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-01-20 20:45:21 +0100

    Add contact part

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const body= document.querySelector('body');
-const content= document.querySelector('.content');
+
+const startLink = document.querySelector('.logo');
+const showsLink = document.querySelector('[href="#shows"]');
+const galleryLink = document.querySelector('[href="#gallery"]');
+const aboutLink = document.querySelector('[href="#about"]');
+const contactLink = document.querySelector('[href="#contact"]');
+
+const start = document.querySelector('.start');
 const shows = document.querySelector('.shows');
 const gallery = document.querySelector('.gallery');
 const about = document.querySelector('.about');
 const contact = document.querySelector('.contact');
 
-contact.addEventListener('click',
-	body.style.background = url('../images/for_web/red.jpg') no-repeat 80% center fixed;
+function showStart() {
+	contact.style.display = 'none';
+	start.style.display = 'block';
+	body.style.background = 'url("images/for_web/violet_wall.jpg") no-repeat center center fixed';
+}
+
+function showContact() {
+	contact.style.display = 'block';
+	start.style.display = 'none';
+	body.style.background = 'black';
+}
+
+startLink.addEventListener('click', showStart);
+contactLink.addEventListener('click', showContact);
