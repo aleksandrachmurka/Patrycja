e3de0fbf04b190d56d4279ff897f262a2b88d975
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-01-20 18:12:08 +0100

    Change meta tags

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const gallery = document.querySelector('.gallery');
 const about = document.querySelector('.about');
 const contact = document.querySelector('.contact');
 
+contact.addEventListener('click',
+	body.style.background = url('../images/for_web/red.jpg') no-repeat 80% center fixed;
