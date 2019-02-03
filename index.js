2596036a2590cfd896661672e56184891aa5fd42
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2019-02-03 20:59:23 +0100

    Fix responsiveness and clean styles

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const body= document.querySelector('body');
 
 const startLink = document.querySelector('.logo');
 const showsLink = document.querySelector('[href="#shows"]');
-const galleryLink = document.querySelector('[href="#gallery"]');
+const fotoLink = document.querySelector('[href="#foto"]');
+const videoLink = document.querySelector('[href="#video"]');
 const aboutLink = document.querySelector('[href="#about"]');
 const contactLink = document.querySelector('[href="#contact"]');
 
 const start = document.querySelector('.start');
 const shows = document.querySelector('.shows');
 const gallery = document.querySelector('.gallery');
+const foto = document.querySelector('.foto');
+const video = document.querySelector('.video');
 const about = document.querySelector('.about');
 const contact = document.querySelector('.contact');
 
@@ -21,8 +24,20 @@ function showStart() {
 	body.style.backgroundSize = 'cover';
 }
 
-function showGallery() {
+function showFoto() {
 	gallery.style.display = 'block';
+	foto.style.display = 'block';
+	video.style.display = 'block';
+	start.style.display = 'none';
+	contact.style.display = 'none';
+	body.style.background = 'black';
+	body.style.backgroundSize = 'cover';
+}
+
+function showVideo() {
+	gallery.style.display = 'block';
+	video.style.display = 'block';
+	foto.style.display = 'none';
 	start.style.display = 'none';
 	contact.style.display = 'none';
 	body.style.background = 'black';
@@ -38,6 +53,7 @@ function showContact() {
 
 
 startLink.addEventListener('click', showStart);
-galleryLink.addEventListener('click', showGallery);
+fotoLink.addEventListener('click', showFoto);
+videoLink.addEventListener('click', showVideo);
 contactLink.addEventListener('click', showContact);
 
